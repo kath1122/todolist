7657128e1cbd6aaedff2133b860f65ec5de931fa
katherine <katherine.tseng@skylinetw.com>
2024-03-02 17:48:08 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import { useCounterStore } from '@/stores/counter'
+const store = useCounterStore()
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <h1>{{ store.name }}</h1>
+  </main>
+</template>
